c1ce3613d4f29016899b8e0472e655bf4e43e346
Nabin Chapagain <nabinchapagain001@gmail.com>
2025-10-17 23:06:34 +0530

    Add Sepolia deployment script and update RPC endpoint
    - Add deploy:sepolia npm script
    - Update hardhat config with Alchemy demo endpoint
    - Prepare for testnet deployment

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -20,7 +20,7 @@ module.exports = {
       url: "http://127.0.0.1:8545",
     },
     sepolia: {
-      url: "https://rpc.sepolia.org",
+      url: "https://eth-sepolia.g.alchemy.com/v2/demo",
       accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
     },
     // mainnet: {
